b9b4985ed62448cd19afcce4da5b2401db035c4d
qjsarawr <jpquiambao@student.hau.edu.ph>
2024-07-27 21:47:50 +0800

    git commit

--- prelims/Server/routes.js
+++ prelims/Server/routes.js
@@ -1,19 +1,33 @@
 var routes = {
   root: function (name) {
-    return 'Welcome ' + name + ' This is an activity about basics of Node.js';
+    return (
+      '<html><body><h1>Welcome to my Node.js Application</h1><br>' +
+      'Welcome ' +
+      name +
+      ' This is an activity about basics of Node.js </body></html>'
+    );
   },
   about: function (name) {
     return (
+      '<html><body><h1>This is the about page.</h1><br>' +
       'Hello ' +
       name +
-      '. This activity will teach on how to deal with a simple server and local modules in Node.js'
+      '. This activity will teach on how to deal with a simple server and local modules in Node.js</body></html>'
     );
   },
   contact: function (name) {
-    return name + ', if you want additional details...';
+    return (
+      '<html><body><h1>This is the Contact Page.</h1><br></body></html>' +
+      name +
+      ', if you want additional details...'
+    );
   },
   gallery: function (name) {
-    return 'Build your gallery here, ' + name;
+    return (
+      '<html><body><h1>This is the Gallery Page.</h1><br></body></html>' +
+      'Build your gallery here, ' +
+      name
+    );
   },
 };
 module.exports = routes;
--- prelims/Server/server.js
+++ prelims/Server/server.js
@@ -5,31 +5,19 @@ var myRoutes = require('./routes.js');
 var server = http.createServer(function (req, res) {
   if (req.url == '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(
-      '<html><body><h1>Welcome to my Node.js Application</h1><br></body></html>' +
-        myRoutes.root('June Sarah')
-    );
+    res.write(myRoutes.root('June Sarah'));
     res.end();
   } else if (req.url == '/about') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(
-      '<html><body><h1>This is the about page.</h1><br></body></html>' +
-        myRoutes.about('June Sarah')
-    );
+    res.write(myRoutes.about('June Sarah'));
     res.end();
   } else if (req.url == '/contact') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(
-      '<html><body><h1>This is the Contact Page.</h1><br></body></html>' +
-        myRoutes.contact('June Sarah')
-    );
+    res.write(myRoutes.contact('June Sarah'));
     res.end();
   } else if (req.url == '/gallery') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(
-      '<html><body><h1>This is the Gallery Page.</h1><br></body></html>' +
-        myRoutes.gallery('June Sarah')
-    );
+    res.write(myRoutes.gallery('June Sarah'));
     res.end();
   } else res.end('Invalid Request');
 });
